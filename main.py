f3a44e29c214de4db3e165b4c581c4649998ab51
santipr05 <santiago.paredes@correo.unimet.edu.ve>
2024-06-29 01:15:18 -0400

    Modelar clase Stadium, Restaurant

--- main.py
+++ main.py
@@ -4,3 +4,28 @@ class Team :
         self.code = code
         self.name = name
         self.group = group
+
+
+class RestaurantItem:
+    def __init__(self, name, price, stock, aditional) -> None:
+        self.name = name
+        self.price = price
+        self.stock = stock
+
+        self.isFood = aditional == "plate"
+        self.isAlcoholic = aditional == "alcoholic"
+
+
+class Restaurant:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.products = []
+
+
+class Stadium:
+    def __init__(self, id, name, city, capacity) -> None:
+        self.id = id
+        self.name = name
+        self.city = city
+        self.capacity = capacity
+        self.restaurants = []
