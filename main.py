70d7135e1720ceee9c3ea12c0737d0fca97f0163
santipr05 <santiago.paredes@correo.unimet.edu.ve>
2024-06-29 11:54:26 -0400

    Crear funcion para obtener los partidos desde la api

--- main.py
+++ main.py
@@ -36,11 +36,12 @@ class Stadium:
 
 
 class Match: 
-    def __init__(self, id, number, date, group, home_id, away_id) -> None:
+    def __init__(self, id, number, date, group, stadium_id, home_id, away_id) -> None:
         self.id = id
         self.number = number
         self.date = date
         self.group = group
+        self.stadium_id = stadium_id
         self.home_id = home_id
         self.away_id = away_id
 
@@ -86,4 +87,24 @@ def getStadiums():
     print(stadiums[0].restaurants[0].products[0].name)
     return stadiums
 
-getStadiums()
+def getMatches():
+    res = requests.get('https://raw.githubusercontent.com/Algoritmos-y-Programacion/api-proyecto/main/matches.json')
+    if(res.status_code != 200):
+        print("No se pudo obtener los datos de los partidos")
+        return
+    
+    data = res.json()
+
+    matches: list[Match] = []
+
+    for m in data: 
+        match = Match(m['id'], m['number'], m['date'], m['group'], m['stadium_id'], m['home']['id'], m['away']['id'])
+
+        matches.append(match)
+
+    print(matches[0].away_id)
+ 
+    return matches
+
+getMatches()
+
