470402047b858e5b4ab26dc7ede617672bb912a3
santipr05 <santiago.paredes@correo.unimet.edu.ve>
2024-06-29 01:14:34 -0400

    Modelar clase Team

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print('hello world')
+class Team :
+    def __init__(self, id, code, name, group) -> None:
+        self.id = id
+        self.code = code
+        self.name = name
+        self.group = group
