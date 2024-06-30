2ca14dd2b3b74754fb96bec5b76e59b3816505a8
santipr05 <santiago.paredes@correo.unimet.edu.ve>
2024-06-29 21:53:13 -0400

    Funcion para comprobar si un numero es vampiro

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import math
 from data_objects import Team, Stadium, Match, Restaurant, RestaurantItem
 
 def getTeams():
@@ -32,7 +33,7 @@ def getStadiums():
     stadiums = []
 
     for s in data:
-        stadium = Stadium(s['id'], s['name'], s['city'])
+        stadium = Stadium(s['id'], s['name'], s['city'], s['capacity'])
         for r in s['restaurants']:
             restaurant = Restaurant(r['name'])
             for i in r['products']:
@@ -101,8 +102,53 @@ class MatchStadiumManager:
 
 
 
-teams = getTeams()
-stadiums = getStadiums()
-matches = getMatches(teams, stadiums)
+# teams = getTeams()
+# stadiums = getStadiums()
+# matches = getMatches(teams, stadiums)
+#
+# matchStadiumManager = MatchStadiumManager(stadiums, matches)
 
-matchStadiumManager = MatchStadiumManager(stadiums, matches)
+
+def isVapireNumber(n):
+    n_digits = math.ceil(math.log10(n))
+    digits = []
+    permutation = []
+
+    for i in range(n_digits):
+        permutation.append(-1)
+
+    if not n_digits % 2 == 0:
+        return False
+
+    def buildNumber(digits):
+        result = 0;
+        for d in digits:
+            result = result * 10 + d
+
+        return result
+
+    def isVampire(digits):
+        half = math.floor(len(digits)/2)
+        a = buildNumber(digits[:half])
+        b = buildNumber(digits[half:])
+        if(a * b == n):
+            return True
+
+    def permutate(digit = 0):
+        if not digit < n_digits:
+            return isVampire(permutation)
+
+        for i in range(n_digits):
+            if i in permutation:
+                continue
+            permutation[digit] = digits[i]
+            if permutate(digit + 1):
+                return True
+            permutation[digit] = -1
+
+        return False
+
+    for i in range(n_digits):
+        digits.append(math.floor(n / math.pow(10, i)) % 10)
+
+    return permutate()
